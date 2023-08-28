f48e51b3c6dd9119c79f132c6fb0ee15739c3ec0
Nazar Pylypiak <pylypyaknazar@gmail.com>
2023-08-28 23:33:03 +0300

    Add rgb effects; Change handedness by pin

--- keyboards/bastardkb/tbkmini/keymaps/jaizer/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/jaizer/keymap.c
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#define _DVORAK 0
 #define _FIRST 1
 #define _SECOND 2
 #define _THIRD 3
@@ -24,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_3x6_3(
         //,-------------------------------------------------------------.                        ,-------------------------------------------------------------.
-              KC_TAB,       KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                              KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,    KC_BSPC,
+              KC_TAB,    KC_QUOT,  KC_COMM,   KC_DOT,     KC_P,     KC_Y,                              KC_F,     KC_G,     KC_C,     KC_R,     KC_L,    KC_BSPC,
         //|---------|  |--------+---------+---------+---------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-             KC_LCTL,       KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                              KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,    KC_QUOT,
+       OSM(MOD_LCTL),       KC_A,     KC_O,     KC_E,     KC_U,     KC_I,                              KC_D,     KC_H,     KC_T,     KC_N,     KC_S,    KC_MINS,
         //|---------|  |--------+---------+---------+---------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-       OSM(MOD_LSFT),       KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                              KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,     KC_ESC,
+       OSM(MOD_LSFT),    KC_SCLN,     KC_Q,     KC_J,     KC_K,     KC_X,                              KC_B,     KC_M,     KC_W,    KC_V,      KC_Z,     KC_ESC,
         //|---------|  |--------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+--------+----------|  |--------|
                                                        KC_LGUI,   KC_SPC,    TT(1),         TT(2),   KC_ENT,   KC_RALT
                                                   //`---------+---------+---------|    |---------+---------+---------|'
@@ -38,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //,-------------------------------------------------------------.                        ,-------------------------------------------------------------.
               KC_TAB,       KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                              KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_BSPC,
         //|---------|  |--------+---------+---------+---------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-             KC_LCTL,    XXXXXXX,  XXXXXXX,  KC_BRMD,  KC_BRMU,  RGB_TOG,                           KC_LEFT,  KC_DOWN,    KC_UP, KC_RIGHT,  RGB_TOG,    KC_RCTL,
+       OSM(MOD_LCTL),    XXXXXXX,  XXXXXXX,  KC_BRMD,  KC_BRMU,  XXXXXXX,                           KC_LEFT,  KC_DOWN,    KC_UP, KC_RIGHT,  XXXXXXX,OSM(MOD_RCTL),
         //|---------|  |--------+---------+---------+---------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-       OSM(MOD_LSFT),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                           KC_MPRV,  XXXXXXX,  KC_MPLY,  KC_MNXT,  XXXXXXX,OSM(KC_RSFT),
+       OSM(MOD_LSFT),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                           KC_MPRV,  XXXXXXX,  XXXXXXX,  KC_MNXT,  XXXXXXX,OSM(KC_RSFT),
         //|---------|  |--------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+--------+----------|  |--------|
                                                        KC_LGUI,   KC_SPC,  _______,         TT(3),   KC_ENT,   KC_RALT
                                                   //`---------+---------+---------|    |---------+---------+---------|'
@@ -48,50 +49,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
         //,----------------------------------------------------------------------,,,,-------.                        ,-------------------------------------------------------------.
-              KC_TAB,    KC_EXLM,    KC_AT,         KC_HASH,            KC_DLR,  KC_PERC,                           KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,    KC_BSPC,
-        //|---------|  |--------+---------+------------------+----------------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-             KC_LCTL,    XXXXXXX,  XXXXXXX, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP,  KC_MUTE,                           KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,  KC_BSLS,     KC_GRV,
-        //|---------|  |--------+---------+------------------+----------------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-       OSM(MOD_LSFT),    XXXXXXX,  XXXXXXX,           XXXXXXX,         XXXXXXX,  XXXXXXX,                           KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,    KC_TILD,
-        //|---------|  |--------+---------+------------------+----------------+---------+----------|    |--------+---------+---------+---------+---------+---------|  |--------|
-                                                                       KC_LGUI,    KC_SPC,   TT(4),       _______,   KC_ENT,   KC_RALT
-                                                                  //`---------+---------+----------|    |--------+---------+---------|'
+              KC_TAB,    KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,                           KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,    KC_BSPC,
+        //|---------|  |--------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+--------+----------|  |--------|
+       OSM(MOD_LCTL),    XXXXXXX,  XXXXXXX,  KC_VOLD, KC_VOLU,  KC_MUTE,                           KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,  KC_BSLS,    KC_SLSH,
+        //|---------|  |--------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+--------+----------|  |--------|
+       OSM(MOD_LSFT),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                           KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,    KC_TILD,
+        //|---------|  |--------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+--------+----------|  |--------|
+                                                       KC_LGUI,   KC_SPC,    TT(4),       _______,   KC_ENT,   KC_RALT
+                                                  //`---------+---------+---------|    |---------+---------+---------|'
         ),
 
     [3] = LAYOUT_split_3x6_3(
         //,-------------------------------------------------------------.                        ,-------------------------------------------------------------.
              XXXXXXX,      KC_F1,      KC_F2,      KC_F3,       KC_F4,    KC_F5,                             KC_F6,    KC_F7,    KC_F8,   KC_F9,    KC_F10,    XXXXXXX,
         //|---------|  |--------+-----------+-----------+------------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-             KC_LCTL,    XXXXXXX, KC_MS_LEFT,   KC_MS_UP, KC_MS_RIGHT,  XXXXXXX,                            KC_F11,  KC_BTN1,  KC_BTN2,  KC_WH_U,  XXXXXXX,    KC_RCTL,
+       OSM(MOD_LCTL),    XXXXXXX, KC_MS_LEFT,   KC_MS_UP, KC_MS_RIGHT,  XXXXXXX,                            KC_F11,  KC_BTN1,  XXXXXXX,  KC_WH_U,  XXXXXXX,OSM(MOD_RCTL),
         //|---------|  |--------+-----------+-----------+------------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-             KC_LSFT,    XXXXXXX, XXXXXXX,    KC_MS_DOWN,     XXXXXXX,  XXXXXXX,                            KC_F12,  XXXXXXX,  XXXXXXX, KC_WH_D,   XXXXXXX,    KC_RSFT,
+       OSM(MOD_LSFT),    XXXXXXX, XXXXXXX,    KC_MS_DOWN,     XXXXXXX,  XXXXXXX,                            KC_F12,  KC_BTN2,  XXXXXXX, KC_WH_D,   XXXXXXX,OSM(KC_RSFT),
         //|---------|  |--------+-----------+-----------+------------+---------+---------|    |---------+---------+---------+---------+---------+---------|  |--------|
                                                        KC_LGUI,    KC_SPC, _______,       _______,   KC_ENT,   KC_RALT
                                                   //`---------+----------+--------|    |---------+---------+---------|'
         ),
     [4] = LAYOUT_split_3x6_3(
         //,-------------------------------------------------------------.                        ,-------------------------------------------------------------.
-             QK_BOOT,    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  XXXXXXX,                           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,
+             QK_BOOT,    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_SPI,                           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,
         //|---------|  |--------+---------+---------+---------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-             KC_SLEP,    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  XXXXXXX,                           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,
+             KC_SLEP,    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_SPD,                           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,
         //|---------|  |--------+---------+---------+---------+---------|                        |---------+---------+---------+---------+---------|  |--------|
-             KC_LCTL,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                           KC_ACL0,  KC_ACL1,  KC_ACL2,  XXXXXXX,  XXXXXXX,    XXXXXXX,
+             XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                           KC_ACL0,  KC_ACL1,  KC_ACL2,  XXXXXXX,  XXXXXXX,    XXXXXXX,
         //|---------|  |--------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+--------+----------|  |--------|
-                                                       KC_LGUI,   KC_SPC,  _______,       _______,   KC_ENT,   KC_RALT
+                                                       XXXXXXX,  XXXXXXX,  _______,       _______,  XXXXXXX,  XXXXXXX
                                                   //`---------+---------+---------|    |---------+---------+---------|'
         )};
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = get_highest_layer(state);
-
     switch (layer) {
     case _FIRST:
         SEND_STRING(SS_TAP(X_F17));
-         rgblight_sethsv(HSV_WHITE);
+        rgblight_sethsv(HSV_GREEN);
         break;
     case _SECOND:
         SEND_STRING(SS_TAP(X_F18));
-         rgblight_sethsv(HSV_PURPLE);
+        rgblight_sethsv(HSV_SPRINGGREEN);
         break;
     case _THIRD:
         SEND_STRING(SS_TAP(X_F19));
@@ -99,14 +99,22 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         break;
     case _FOURTH:
         SEND_STRING(SS_TAP(X_F20));
-         rgblight_sethsv(HSV_RED);
+        rgblight_sethsv(HSV_RED);
+//         rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE + 0);
         break;
     default:
         SEND_STRING(SS_TAP(X_F16));
-         rgblight_sethsv(HSV_GREEN);
+        rgblight_sethsv(RGB_BLUE);
         break;
     }
   return state;
+}
+
+void keyboard_post_init_user(void) {
+  // Call the post init code.
+  rgblight_enable_noeeprom(); // enables Rgb, without saving settings
+  rgblight_sethsv_noeeprom(RGB_BLUE); // sets the color to teal/cyan without saving
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_KNIGHT + 2); // sets mode to Fast breathing without saving
 }
 
 
--- keyboards/bastardkb/tbkmini/v1/elitec/config.h
+++ keyboards/bastardkb/tbkmini/v1/elitec/config.h
@@ -17,13 +17,14 @@
  */
 
 #pragma once
-
 /* Handedness. */
-#define MASTER_RIGHT
-#define SPLIT_USB_DETECT
+// #define MASTER_RIGHT
+#define SPLIT_HAND_PIN_LOW_IS_LEFT
+#define SPLIT_HAND_PIN F4
+// #define SPLIT_USB_DETECT
 // #define EE_HANDS
 #define TAPPING_TOGGLE 3
-#define ONESHOT_TIMEOUT 2500
+#define ONESHOT_TIMEOUT 5000
 #ifdef RGBLIGHT_ENABLE
 #    undef RGBLED_NUM
 #    undef RGBLED_SPLIT
@@ -31,12 +32,20 @@
 #    define RGBLIGHT_SPLIT
 #    define RGBLED_NUM 12
 #    define RGBLED_SPLIT {6, 6}
-#    define RGBLIGHT_DEFAULT_HUE 180
-#    define RGBLIGHT_LIMIT_VAL 180
-#    define RGBLIGHT_DEFAULT_SAT 180
+#    define RGBLIGHT_DEFAULT_HUE 255
+#    define RGBLIGHT_LIMIT_VAL 128
+#    define RGBLIGHT_DEFAULT_SAT 128
 #    define RGBLIGHT_SLEEP
-#    define RGBLIGHT_HUE_STEP 25
-#    define RGBLIGHT_SAT_STEP 25
-#    define RGBLIGHT_VAL_STEP 25
+#    define RGBLIGHT_HUE_STEP 12
+#    define RGBLIGHT_SAT_STEP 12
+#    define RGBLIGHT_VAL_STEP 12
+// #    define RGBLIGHT_EFFECT_ALTERNATING
+// #    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_CHRISTMAS
+// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #    define RGBLIGHT_EFFECT_SNAKE
+// #    define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_MODE_KNIGHT
+#    define RGBLIGHT_EFFECT_BREATHING
 #endif
-
